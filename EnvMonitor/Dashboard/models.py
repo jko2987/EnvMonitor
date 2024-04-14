1e9adfa936d0f3f9c4f2dfdc6843e3dfefc88819
Jason Westover <54950781+jko2987@users.noreply.github.com>
2024-04-14 11:48:16 -0400

    added 3 data tables

--- EnvMonitor/Dashboard/models.py
+++ EnvMonitor/Dashboard/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class PlantSpecimen(models.Model):
+    plant_date = models.DateTimeField()
+    plant_type = models.ForeignKey('PlantType')
+    
+class PlantType(models.Model):
+    pass
+    
+class EnvReading(models.Model):
+    pass
